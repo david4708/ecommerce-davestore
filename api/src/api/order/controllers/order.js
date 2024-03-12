a4bd408c84b1acef5504c5758f359db704d3cd51
david4708 <yepesdavid@hotmail.com>
2024-03-12 13:01:00 -0500

    nextcommit :rocket:

--- api/src/api/order/controllers/order.js
+++ api/src/api/order/controllers/order.js
@@ -9,29 +9,33 @@ const { createCoreController } = require("@strapi/strapi").factories;
 module.exports = createCoreController("api::order.order", ({ strapi }) => ({
   async create(ctx) {
     const { products } = ctx.request.body;
-    const lineItems = await Promise.all(
-      products.map(async (product) => {
-        const item = await strapi
-          .service("api::product.product")
-          .findOne(product.id);
 
-        return {
-          price_data: {
-            currency: "usd",
-            product_data: {
-              name: item.title,
-            },
-            unit_amount: item.price * 100,
-          },
-          quantity: item.quantity,
-        };
-      })
-    );
     try {
+      const lineItems = await Promise.all(
+        products.map(async (product) => {
+          const item = await strapi
+            .service("api::product.product")
+            .findOne(product.id);
+
+          return {
+            price_data: {
+              currency: "usd",
+              product_data: {
+                name: item.title,
+              },
+              unit_amount: item.price * 100,
+            },
+            quantity: item.quantity,
+          };
+        })
+      );
       const session = await stripe.checkout.sessions.create({
         mode: "payment",
         success_url: process.env.CLIENT_URL + "?success=true",
-        cancel_url: process.env.CLIENT_URL + "?success=false",
+        cancel_url: process.env.CLIENT_URL + "?canceled=false",
+
+        /* success_url: `${process.env.CLIENT_URL}?success=true`,
+        cancel_url: `${process.env.CLIENT_URL}?canceled=true`, */
         line_items: lineItems,
         shipping_address_collection: { allowed_countries: ["US", "CA"] },
         payment_method_types: ["card"],
